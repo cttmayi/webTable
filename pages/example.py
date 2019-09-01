5a535ffdc0a2b2f32db4130d1ac9642db18155bd
cttmayi <cttmayi@hotmail.com>
2019-09-02 01:06:33 +0800

    comment for example

--- pages/example.py
+++ pages/example.py
@@ -6,57 +6,52 @@ from database.dbdummy import DbDummy
 
 import module.datatables as dt
 
+#db = DbElasticsearch() # 本代码支持使用Elasticsearch
+db = DbDummy() # Dummy数据库(使用), 用于调试
 
-#db = DbElasticsearch()
-db = DbDummy()
-
-DB = 'example'
-db.create(DB)
+DB = 'example' # 数据库名称
+db.create(DB) # 如没有数据库, 创建数据库
 
 
 def html(p0):
     ret = {}
 
     datatables = {}
-    datatables['id'] = 'itemid'
-    datatables['order'] = [[ 1, "desc" ]]
-    datatables['number'] = True
-    datatables['scrollY'] = 'window.screen.availHeight'
-    #datatables['pageLength'] = 25 # 10, 25, 50, 100
-    datatables['th'] = []
-    datatables['th'].append(dt.th('itemid', 'itemid', "100px"))
-    datatables['th'].append(dt.th('productid', 'productid', "200px"))
-    datatables['th'].append(dt.th('listprice', 'listprice', "200px", 'edit'))
-    datatables['th'].append(dt.th('unitcost', 'unitcost', "200px", 'select', ['100', '200']))
-    datatables['th'].append(dt.th('attr1', 'attr1', "200px"))
-    datatables['th'].append(dt.th('status', 'status'))
+    datatables['id'] = 'itemid' # 唯一 field ID, 用于修改数据
+    datatables['order'] = [[ 1, "desc" ]] # 默认的排列(第2列, 降序)
+    datatables['number'] = True # 支持第1列自动序列
+    datatables['scrollY'] = 'window.screen.availHeight' # 是否支持垂直scroll(如不支持, 注释即可), 可用javacript代码, 或者数字(600, 800等).
+    #datatables['pageLength'] = 25 #  # 是否支持翻页(如不支持, 注释即可), 支持页面数: 10, 25, 50, 100
+    datatables['th'] = [] # 用于列描述
+    datatables['th'].append(dt.th('ID', 'itemid', "100px")) # 列宽度为100px, 第1个参数为显示名, 第2参数为Key名称.  
+    datatables['th'].append(dt.th('PID', 'productid', "200px"))
+    datatables['th'].append(dt.th('Price', 'listprice', "200px", 'edit')) # edit: 可编辑
+    datatables['th'].append(dt.th('Cost', 'unitcost', "200px", 'select', ['100', '200'])) # select: 可选择
+    datatables['th'].append(dt.th('Attr', 'attr1', "200px"))
+    datatables['th'].append(dt.th('Status', 'status'))
 
-    ret['title'] = 'CQ'
+    ret['title'] = 'CQ' # Web 标题
     ret['datatables'] = datatables
 
     #ret['template'] = 'example'
     return ret
 
 
-
-
 def query(p0):
     ret = db.query(DB)
-
     return ret
 
 def update(p0, db_id, field, value):
     body = {
         field: value
     }
-
     db.update(DB, db_id, body)
     return True
 
 
 
 ##########################################################
-
+# 范例 Dummy数据库的内容插入
 def insert(db, id, price):
     data = {
         'itemid' : str(id),
@@ -66,9 +61,7 @@ def insert(db, id, price):
         'attr1' : 'A',
         'status' : 'O',
     }
-
     id = db.insert('example', data, id)
-
 
 for i in range(100):
     insert(db, 'TV.'+str(i), 1000)
