bb0a7f49398eb333c0c70eba4f20d25a286a3d9d
cttmayi <cttmayi@hotmail.com>
2020-07-25 03:04:08 +0800

    width=*, min-width=200px

--- pages/configuration.py
+++ pages/configuration.py
@@ -37,7 +37,7 @@ def html(p0, p1):
     vuetables['th'].append(dt.th('Name', 'name', "200px", 'edit'))
     vuetables['th'].append(dt.th('Key/Index', 'key', "200px", 'edit'))
     vuetables['th'].append(dt.th('Value', 'value', "200px", 'edit'))
-    vuetables['th'].append(dt.th('Comment', 'commnet', '400px', 'edit'))
+    vuetables['th'].append(dt.th('Comment', 'commnet', '*', 'edit'))
 
 
     ret['title'] = 'Configuration'
