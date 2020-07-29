da582db8376c93de1caf5432159a50a6e87394ff
cttmayi <cttmayi@hotmail.com>
2020-07-29 22:54:24 +0800

    add comment

--- pages/example.py
+++ pages/example.py
@@ -13,32 +13,32 @@ def html(p0, p1):
     ret = {}
 
     vuetables = {}
-    vuetables['id'] = 'itemid' # 唯一 field ID, 用于修改数据
-    vuetables['default_sort'] = {'field': 'itemid', 'order': 'desc'} # 排序，asc/desc
-    vuetables['toolbar'] = ['insert', 'delete', 'export'] # 配置 Toolbar 按钮
+    vuetables['id'] = 'itemid' # 唯一 field ID, 用于修改数据时提供的标识
+    vuetables['default_sort'] = {'field': 'itemid', 'order': 'desc'} # 默认排序设定，asc/desc
+    vuetables['toolbar'] = ['insert', 'delete', 'export'] # 配置 Toolbar 按钮，分别是添加，删除，导出CVS按钮
     vuetables['height'] = 'window.innerHeight - 150' # '700' # 设定表格高度, 可用javacript代码, 或者字符串('600'等).
 
     vuetables['extend'] = {
-        'value': '{[ row.listprice ]}'
+        'value': '{[ row.listprice ]}' # 展开功能， 用VUE表达式，展开后的显示数据 
     }
 
     vuetables['th'] = [
         {'name': 'ID', 'field': 'itemid', 'width': '100', # name为显示名, field为Key名称. width为100,
-            'fixed': 'left', 'sortable': 'false'}, # fixed 表示是否固定
+            'fixed': 'left', 'sortable': 'false'}, # fixed 表示是否固定， sortable 默认为true，如设定为false表示不能排序（无排序按钮）
 
         {'name': 'PID', 'field': 'productid', 'width': '200',
-            'editor': 'select', 'options': [
+            'editor': 'select', 'options': [ # select，选项
                 { 'label': '', 'value': '' }, # label为选项显示名称， value为保存数据 
                 { 'label': '产品1', 'value': 'P1' }, 
                 { 'label': '产品2', 'value': 'P2' }, 
                 { 'label': '产品3', 'value': 'P3' }, 
             ],},
         {'name': 'Price', 'field': 'listprice', 'width': '200', 'editor': 'edit',
-            'tooltip': {'value': '"COST: " + row.unitcost '}
+            'tooltip': {'value': '"COST: " + row.unitcost '} # tooltip 显示
         },
         {'name': 'Cost', 'field': 'unitcost', 'width': '200',
             'editor': 'select', 'options': ['', '100', '200', '300'], # Options 简洁表达方式，lable和value一致
-            'style': [{'cond': "row.unitcost > 150", 'style': 'bg-green'}]},
+            'style': [{'cond': "row.unitcost > 150", 'style': 'bg-green'}]}, # style 条件判断， 设定颜色（bg-red，bg-yellow，bg-green，bg-blue）
         {'name': 'Status', 'field': 'status', 'width': '*', 'editor': 'textarea', 
             'search': 'false'}, # 字段默认可搜索， 设定不可搜索（'search': 'false'）
     ]
